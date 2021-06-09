3d4f6968b79a596622bec60383a4626744e7649e
Varejjjka <smarmel@inbox.ru>
2021-06-09 21:10:36 +0200

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage()) ;
+
+        boolean comparison = bean == bean1;
+        boolean comparison1 = bean2 == bean3;
+
+        System.out.println(comparison);
+        System.out.println(comparison1);
     }
 }
